1f1fd12f193a7c4596ea0150decc66d0f98b9173
Dubrovskirill <zet10set11@gmail.com>
2024-03-05 12:59:55 +0700

    Создание функции CreateFiles и ShiftFiles

--- Polyphase merge sort/Polyphase merge sort/polyphase.cpp
+++ Polyphase merge sort/Polyphase merge sort/polyphase.cpp
@@ -6,6 +6,37 @@
 #include<vector>
 #include<iterator>
 
+std::string* CreateFiles(int filesNumber)
+{
+	std::string* fileName = new std::string[filesNumber];
+	for (int i = 0; i < filesNumber; ++i)
+	{
+		std::string s = "file_" + std::to_string(i) + ".txt";
+		fileName[i] = s;
+		std::ofstream file(s);
+		file.close();
+	}
+	return fileName;
+}
+
+void FreeMemory(std::string* fileName)
+{
+	delete[] fileName;
+}
+
+void ShiftFiles(std::string* fileName, int filesNumber)
+{
+	if (rename(fileName[filesNumber - 1].c_str(), "temp.txt"))
+		std::cout << "erorr!";
+	for (int i = filesNumber - 2; i >= 0; --i)
+	{
+		if (rename(fileName[i].c_str(), fileName[i + 1].c_str()))
+			std::cout << "erorr!";
+	}
+	if (rename("temp.txt", fileName[0].c_str()))
+		std::cout << "erorr!";
+}
+
 int main()
 {
 
